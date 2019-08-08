a3d86c0ea7fb105d4254f35058bf4010c453a1c9
Santos, Alfredo <alfredo.santos@br.experian.com>
2019-08-08 19:25:56 -0300

    Estacionamento

--- java-7/src/main/java/challenge/Estacionamento.java
+++ java-7/src/main/java/challenge/Estacionamento.java
@@ -2,7 +2,10 @@ package challenge;
 
 public class Estacionamento {
 
-
+    private static final int MAX_PONTOS = 20;
+    private static final int IDADE_MIN = 18;
+    private static final int SENIOR = 55;
+    private static final int MAX_VAGAS = 10;
 
     public void estacionar(Carro carro) {
       
